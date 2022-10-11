d99e6a0525fa5091fc7319354d71c7a5b47370f0
Manuel Amstutz <manuel.amstutz@orphis.ch>
2022-10-12 00:53:32 +0200

    Simple openid-connect Implicit flow demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Microsoft.AspNetCore.WebUtilities;
 
 var app = WebApplication.CreateBuilder(args).Build();
@@ -20,6 +22,9 @@ app.MapGet("/", (context) =>
 		{ "scope", "openid profile email" },
 		{ "response_mode", "form_post" },
 		{ "nonce", "SomValueNeedsToBeValidatedByTheAuthHandler" },
+
+		// here you can encode everything that is required in your handler - ae the redirect URL - often base64 encoded
+		{ "state", Convert.ToBase64String(Encoding.UTF8.GetBytes("https://MyPageAfterLogin")) }
 	};
 	context.Response.Redirect(new Uri(QueryHelpers.AddQueryString(authorizeEndpoint, param)).ToString());
 	return Task.CompletedTask;
@@ -41,15 +46,20 @@ app.MapPost("/signin-oidc", (context) =>
 {
 	var idToken = context.Request.Form["id_token"];
 	var scope = context.Request.Form["scope"];
+	var state = context.Request.Form["state"];
 
 	var jwtToken = new JwtSecurityToken(jwtEncodedString: idToken);
+
+	var initiatorUri = Encoding.UTF8.GetString(Convert.FromBase64String(state));
 
 	// check nonce
 	if (jwtToken.Payload.Nonce != "SomValueNeedsToBeValidatedByTheAuthHandler")
 	{
 		return context.Response.WriteAsync($"Nonce Not valid");
 	}
-	return context.Response.WriteAsync($"Id Token Received: {jwtToken}");
+	return context.Response.WriteAsync($"Id Token Received: {jwtToken}\r\n\r\nRedirect To: {initiatorUri}");
+	
+	// normally your application redirects here to the URL stored in state - the URL that triggered the flow
 });
 
 app.Run();
